3a8342f3e0f39541d578f96232ffe2842b310683
Shahbaz181 <shahbaz18619152gmail.com>
2025-08-24 02:22:20 +0530

    Day-1 learning

---
+++ Day 1/first.js
@@ -0,0 +1,36 @@
+console.log("Learning JavaScript");
+console.log("Hello  I'm Syed Shahbaz");
+
+fullname = "Tom Holland";
+console.log(fullname);
+
+fullname = "Tom Holland";
+age = "30";
+Profession = "Actor"
+console.log(fullname,age,Profession);
+
+
+x=null;
+y=undefined;
+console.log(x);
+console.log(y);
+
+
+isFollow = true;
+console.log(isFollow);
+
+isunFollow= false;
+console.log(isunFollow);
+
+
+age=52;
+console.log(fullname,age,Profession);
+
+
+let myname= ("What are you doing");
+
+console.log(myname);
+
+
+var my=("Syed Shahbaz");
+console.log(my);
